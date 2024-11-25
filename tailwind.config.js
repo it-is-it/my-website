a5ceb6594a825d95bda3ccefae3a65d7001b48a9
Ishwor <ishwortimalsina39@gmail.com>
2024-11-25 14:37:59 +0545

    new things

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Include all JS, JSX, TS, and TSX files in the src folder
+    './public/index.html', // Include the main HTML file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
