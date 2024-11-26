be9ea13445da70269ebd0dd6aeb02b9da04d531e
Ishwor <ishwortimalsina39@gmail.com>
2024-11-26 22:15:59 +0545

    javascript portfolio

--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-// src/pages/_app.tsx
-import { AppProps } from 'next/app';
-import '../styles/globals.css'; // Import the global CSS file
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
